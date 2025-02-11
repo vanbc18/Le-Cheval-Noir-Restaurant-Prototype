435045c8b68b9ce2a75ed37914e52524b2d2077e
vanbc18 <vanbui18@hotmail.fr>
2025-02-11 19:24:01 +0100

    On branch main
    Changes committed:
    	new file:   images/black-horse-ai.jpg
    	new file:   images/couscous-ai-2.jpeg
    	new file:   images/le-cheval-noir-assiette.jpg
    	new file:   images/le-cheval-noir-bar-2.jpg
    	new file:   images/le-cheval-noir-bar-3.jpg
    	new file:   images/le-cheval-noir-bar.jpg
    	new file:   images/le-cheval-noir-couscous-hors-assiette.jpg
    	new file:   images/le-cheval-noir-couscous.jpg
    	new file:   images/le-cheval-noir-fresque.jpg
    	new file:   images/le-cheval-noir-salle.jpg
    	new file:   images/le-cheval-noir-semoule.jpg
    	modified:   index.html
    	modified:   script.js
    	modified:   style.css

--- script.js
+++ script.js
@@ -3,3 +3,48 @@ const getYear = () => {
     document.getElementById('year').innerHTML = new Date().getFullYear();
 }
 getYear();
+
+const swiper = new Swiper('.swiper', {
+    speed: 5000,
+    spaceBetween: 0,
+    centeredSlides: true,
+    centeredSlidesBounds: false,
+    spaceBetween: 40,
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1024: {
+        slidesPerView: 4
+      },
+    },
+    lazyPreloadPrevNext: 4,
+    freeMode: {
+        enabled: true,
+      },
+    effect: "slide",
+     mousewheel: {
+        inverse: true,
+      },
+    controller: {
+        inverse: true,
+      },
+      followFinger: true,
+    loop: true,
+    slideToClickedSlide: true,
+    threshold: 5,
+    toucheReleaseOnEdges: true,
+    allowTouchMove: true,
+    cssmode: true
+  });
+
+  
+  window.addEventListener('scroll', function() {
+    const slideHeight = 250; // Hauteur des diapositives
+    const scrollTop = window.scrollY;
+    const slideIndex = Math.round(scrollTop / slideHeight);
+    swiper.slideTo(slideIndex);
+  });
